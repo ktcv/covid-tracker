48faff671e1754eed6f774217f387c05ffa8ca33
Kevin Chen <ktchenvideo@gmail.com>
2020-10-24 13:06:22 +1100

    Updates

---
+++ precache-manifest.9a75edc3cdfce38f557e55bc6cd3e66d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d54ba85e3818abfdf5e78e00c61489",
+    "url": "/live-covid-heatmap/index.html"
+  },
+  {
+    "revision": "e06170e8b4b9eba4e0f8",
+    "url": "/live-covid-heatmap/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "49b42824f40836905de9",
+    "url": "/live-covid-heatmap/static/css/main.3fa6297e.chunk.css"
+  },
+  {
+    "revision": "e06170e8b4b9eba4e0f8",
+    "url": "/live-covid-heatmap/static/js/2.485ea22d.chunk.js"
+  },
+  {
+    "revision": "0b110a718d0a8fc6b16693cbaab585fa",
+    "url": "/live-covid-heatmap/static/js/2.485ea22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b42824f40836905de9",
+    "url": "/live-covid-heatmap/static/js/main.bb7cfc7e.chunk.js"
+  },
+  {
+    "revision": "33a914f683d7939953bd",
+    "url": "/live-covid-heatmap/static/js/runtime-main.15817406.js"
+  }
+]);
